3b1872d0067e1cd061f9b48d3357db07aef73f94
IrinaP <irpetrovamex@gmail.com>
2022-08-10 00:37:54 +0400

    Create MyList (Method - Add and Delete)

---
+++ dynamic array List/MyList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dynamic_array_List
+{
+    internal class MyList<T>
+    {
+        private T[] unit;
+        public int Count { get; set; }
+
+        public int index = 0;
+
+        private int size = 0;
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+
+        public MyList()
+        {
+            IncreaseSize();
+            this.unit = new T[this.size];
+        }
+
+        private int IncreaseSize()
+        { return this.size = this.size + 1; }
+
+        public void Add(T value)
+        {
+            if(this.index == this.size) Array.Resize<T>(ref this.unit, IncreaseSize());
+            this.unit[index] = value;
+            this.index++;
+        }
+
+        public void Delete(int i)
+        {
+            T[] newUnit = new T[this.size];
+            Array.ConstrainedCopy(this.unit, 0, newUnit, 0, i);
+            Array.ConstrainedCopy(this.unit, i+1, newUnit, i, this.size-(i+1));
+            Array.Resize<T>(ref newUnit, Size - 1);
+            this.unit = newUnit;
+            this.index = this.index - 1;
+            this.size = this.size - 1;
+        }
+    }
+}
---
+++ dynamic array List/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace dynamic_array_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(10);
+            myList.Add(20);
+            myList.Add(33);
+            myList.Add(44);
+            myList.Delete(0);
+            myList.Add(5);
+            myList.Add(6);
+            myList.Add(7);
+            myList.Add(8);
+            //myList.Delete(0);
+        }
+    }
+}
