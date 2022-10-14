e9db475360b1e2c4d6345f6a5ac992b72b824b92
IrinaP <irpetrovamex@gmail.com>
2022-10-14 19:54:21 +0400

    Create BotTelegram

---
+++ BotTelegram/DataBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace BotTelegram
+{
+    internal class DataBase
+    {
+        public static void TestConnection()
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                con.Open();
+                if(con.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Подключение установлено!");
+                }
+                SeeAll(con);
+            }
+        }
+        //static NpgsqlConnection baseConnections;
+        private static NpgsqlConnection GetConnection()
+        {
+            NpgsqlConnection baseConnections = new NpgsqlConnection(@$"{ConfigurationManager.ConnectionStrings["DataBase"].ConnectionString}");
+            return baseConnections;
+        }
+
+        static public void SeeAll(NpgsqlConnection con)
+        {
+            NpgsqlDataReader dataReader = null;
+
+            try
+            {
+                NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM componentsdatabase WHERE Key LIKE '101'", con);
+                dataReader = command.ExecuteReader();
+                
+                while (dataReader.Read())
+                {
+                    Console.WriteLine(Convert.ToString(dataReader["Name"]) + Convert.ToString(dataReader["actionontheproduct"]));
+                     
+                }
+                
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                if(dataReader != null && !dataReader.IsClosed)
+                {
+                    dataReader.Close();
+                }
+            }
+
+        }
+    }
+}
---
+++ BotTelegram/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using static BotTelegram.SearchText;
+using System.Configuration;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.OCR;
+
+namespace BotTelegram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Лучше тоже как обработчик события "Старт программы"
+            DataBase.TestConnection();
+            var client = new TelegramBotClient(ConfigurationManager.ConnectionStrings["TelegramBot"].ConnectionString);
+            client.StartReceiving(MyUpdate, Error, null);
+            
+            
+
+            Console.ReadLine();
+        }
+
+        async static Task MyUpdate(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+
+            ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+            {
+                new KeyboardButton[] { "One", "Two" },
+                new KeyboardButton[] { "Three", "Four" },
+            })
+            {
+                ResizeKeyboard = true
+            };
+
+            //Message sentMessage = await botClient.SendTextMessageAsync(
+            //    chatId: message.Chat.Id,
+            //    text: "Choose a response",
+            //    replyMarkup: replyKeyboardMarkup);
+
+            if (message != null)
+            {
+                Console.WriteLine($"{message.Chat.FirstName} -> {message.Text}");
+
+                try
+                {
+                    if (message.Text.ToLower().Contains("привет") || message.Text.ToLower().Contains("ghbdtn"))
+                    {
+                        //DataBase.SeeAll();
+                        await botClient.SendTextMessageAsync(message.Chat.Id, $"Приветики");
+                        return;
+                    }
+
+                    if (message.Text.ToLower().Contains("two"))
+                    {
+                        
+                        //await botClient.SendTextMessageAsync(message.Chat.Id, $"Приветики");
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            if (message.Photo != null)
+            {
+                //СДЕЛАТЬ ЭТО КАК СОБЫТИЕ!!!!!
+                var fileId = update.Message.Photo.Last().FileId;
+                var fileInfo = await botClient.GetFileAsync(fileId);
+                var filePath = fileInfo.FilePath;
+
+                string destinationFilePath = $@"C:\Emgu\Photo\user\{message.From.FirstName}.png";
+                await using FileStream fileStream = System.IO.File.OpenWrite(destinationFilePath);
+                await botClient.DownloadFileAsync(filePath, fileStream);
+                fileStream.Close();
+
+                Tesseract tess = new Tesseract(@"C:\Emgu\langu", "rus", OcrEngineMode.TesseractLstmCombined);
+                tess.SetImage(new Image<Bgr, byte>(destinationFilePath));
+                tess.Recognize();
+                string text = tess.GetUTF8Text();
+
+                tess.Dispose();
+                await botClient.SendTextMessageAsync(message.Chat.Id, $"{text}");
+                return;
+            }
+
+            
+        }
+
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new Exception("Это ошибка");
+        }
+
+
+    }
+}
